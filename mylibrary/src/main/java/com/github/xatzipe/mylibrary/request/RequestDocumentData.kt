8d93f0fb8740d57b10d3a1938d5e1aeb34223b3c
Theodore Chatziperis <xatzipe@gmail.com>
2023-05-25 17:50:46 +0300

    comments

--- mylibrary/src/main/java/com/github/xatzipe/mylibrary/request/RequestDocumentData.kt
+++ mylibrary/src/main/java/com/github/xatzipe/mylibrary/request/RequestDocumentData.kt
@@ -1,7 +1,13 @@
 package com.github.xatzipe.mylibrary.request
 
 data class RequestDocumentData(
+    /**
+     *
+     */
     val identityCredentialName: String,
+    /**
+     *
+     */
     val docType: String,
     val docName: String,
     val userAuthentication: Boolean,
