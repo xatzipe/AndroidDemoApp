9479309789e246d2aab784973542a0a8ffc1ded4
Theodore Chatziperis <xatzipe@gmail.com>
2023-07-19 16:02:44 +0300

    revert version. test pre-build script

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo "This is an App Center Pre-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+(echo ""; echo "org.gradle.java.home=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
